bf0ce0d00acf0c29ac831e29ed1caac6f4ff7383
Andras Pal <andr4s.pal@gmail.com>
2021-01-06 12:36:15 +0000

    Initial Commit

---
+++ CustomSwiftUISegmentedControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSwiftUISegmentedControl
+//
+//  Created by Andras Pal on 06/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSwiftUISegmentedControl/CustomSwiftUISegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUISegmentedControlApp.swift
+//  CustomSwiftUISegmentedControl
+//
+//  Created by Andras Pal on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUISegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
