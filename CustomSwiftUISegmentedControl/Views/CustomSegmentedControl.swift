7df1ff3b6aa107cbab49ad7872e96ff22eece93d
Andras Pal <andr4s.pal@gmail.com>
2021-01-06 14:55:28 +0000

    Add SegmentLabel subview, add the labels to the segmentedControl

--- CustomSwiftUISegmentedControl/Views/CustomSegmentedControl.swift
+++ CustomSwiftUISegmentedControl/Views/CustomSegmentedControl.swift
@@ -10,7 +10,8 @@ struct CustomSegmentedControl: View {
     
     // # Private/Fileprivate
     private let size: CGSize
-    
+    private let segmentLabels: [String]
+
     // # Body
     var body: some View {
         
@@ -21,14 +22,21 @@ struct CustomSegmentedControl: View {
                 .frame(width: size.width, height: size.height)
                 .foregroundColor(.gray)
                 .opacity(0.2)
+            
+            HStack {
+                ForEach(0..<segmentLabels.count) { idx in
+                    SegmentLabel(title: segmentLabels[idx], width: (size.width - 40) / 3, textColour: Color.black)
+                }
+            }
         }
     }
     
     //=======================================
     // MARK: Public Methods
     //=======================================
-    public init(size: CGSize) {
+    public init(size: CGSize, segmentLabels: [String]) {
         self.size = size
+        self.segmentLabels = segmentLabels
     }
     
     //=======================================
@@ -38,10 +46,38 @@ struct CustomSegmentedControl: View {
 
 
 //=======================================
+// MARK: Subviews
+//=======================================
+fileprivate struct SegmentLabel: View {
+    
+    //------------------------------------
+    // MARK: Properties
+    //------------------------------------
+    // # Public/Internal/Open
+    let title: String
+    let width: CGFloat
+    let textColour: Color
+    
+    // # Private/Fileprivate
+    
+    // # Body
+    var body: some View {
+        
+        Text(title)
+            .multilineTextAlignment(.center)
+            .fixedSize(horizontal: false, vertical: false)
+            .foregroundColor(textColour)
+            .frame(width: width)
+            .contentShape(Rectangle())
+    }
+}
+
+
+//=======================================
 // MARK: Previews
 //=======================================
 struct CustomSegmentedControl_Previews: PreviewProvider {
     static var previews: some View {
-        CustomSegmentedControl(size: CGSize(width: UIScreen.main.bounds.width - 40, height: 48))
+        CustomSegmentedControl(size: CGSize(width: UIScreen.main.bounds.width - 40, height: 48), segmentLabels: ["One", "Two", "Three"])
     }
 }
