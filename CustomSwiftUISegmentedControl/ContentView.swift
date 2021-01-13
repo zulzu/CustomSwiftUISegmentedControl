8a0399633868c8b8c9f7941eb8a93890ca625216
Andras Pal <andr4s.pal@gmail.com>
2021-01-13 10:31:17 +0000

    Add some basic content to ContentView so the segmented control can switch between views

--- CustomSwiftUISegmentedControl/ContentView.swift
+++ CustomSwiftUISegmentedControl/ContentView.swift
@@ -11,12 +11,33 @@ struct ContentView: View {
     // # Private/Fileprivate
     // The selected segment of the Segmented Control
     @State private var segmentSelection: Int = 0
+    // The padding value used for the view
     private var mainPadding: CGFloat = 20
     
     // # Body
     var body: some View {
         
-        CustomSegmentedControl(selection: $segmentSelection, size: CGSize(width: UIScreen.main.bounds.width - (mainPadding * 2), height: 48), segmentLabels: ["One", "Two", "Three", "Four"])
+        VStack {
+            
+            CustomSegmentedControl(selection: $segmentSelection, size: CGSize(width: UIScreen.main.bounds.width - (mainPadding * 2), height: 48), segmentLabels: ["One", "Two", "Three", "Four"])
+                .padding(.top, mainPadding)
+            
+            Spacer()
+            
+            if segmentSelection == 0 {
+                Text("View One")
+            } else if segmentSelection == 1 {
+                Text("View Two")
+            } else if segmentSelection == 2 {
+                Text("View Three")
+            } else if segmentSelection == 3 {
+                Text("View Four")
+            } else {
+                Text("Content missing")
+            }
+            
+            Spacer()
+        }
     }
     
     //=======================================
